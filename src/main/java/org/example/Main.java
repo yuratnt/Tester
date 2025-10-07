f4682bf8c14282d40a5baa73a77a54f2e006a98f
yuratnt <starkov-07@list.ru>
2025-10-07 20:33:27 +0500

    Start work

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://practice-automation.com/form-fields/");
+
+        WebElement element = driver.findElement(By.id("name-input"));
+        System.out.println(element);
+
+        System.out.println(element.getAttribute("clientWidth"));
+    }
+}
