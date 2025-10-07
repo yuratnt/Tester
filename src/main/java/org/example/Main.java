a6123f29575eb8bfc063496b6114fb34f1fd3328
yuratnt <starkov-07@list.ru>
2025-10-07 20:37:54 +0500

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;g
 
 import java.util.List;
 
