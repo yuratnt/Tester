357358319ff0caaa592ad7d5a70072e239f82664
yuratnt <starkov-07@list.ru>
2025-10-13 21:04:35 +0500

    popum update

--- src/main/java/org/example/Page/Popup.java
+++ src/main/java/org/example/Page/Popup.java
@@ -1,4 +1,19 @@
 package org.example.Page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Popup {
+
+    WebDriver driver = new FirefoxDriver();
+    Actions actions = new Actions(driver);
+
+    public Popup() {
+        driver.get("https://practice-automation.com/popups/");
+
+    }
+
+
+
 }
--- src/main/java/org/example/Tests/PopupTest.java
+++ src/main/java/org/example/Tests/PopupTest.java
@@ -1,4 +1,6 @@
 package org.example.Tests;
 
 public class PopupTest {
+
+
 }
