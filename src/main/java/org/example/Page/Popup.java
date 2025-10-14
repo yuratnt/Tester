6f42918c58e6d9df898bb354763d0e5b0c3070f6
yuratnt <starkov-07@list.ru>
2025-10-14 10:45:01 +0500

    popup add

--- src/main/java/org/example/Page/Popup.java
+++ src/main/java/org/example/Page/Popup.java
@@ -1,6 +1,8 @@
 package org.example.Page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -9,11 +11,26 @@ public class Popup {
     WebDriver driver = new FirefoxDriver();
     Actions actions = new Actions(driver);
 
+    WebElement alert;
+    WebElement button2;
+    WebElement button3;
     public Popup() {
         driver.get("https://practice-automation.com/popups/");
+    }
+
+    public void alert() {
+        alert = driver.findElement(By.id("alert"));
+        alert.click();
+        driver.switchTo().alert().accept();
+    }
+
+    public void button2() {
 
     }
 
+    public void button3() {
+
+    }
 
 
 }
--- src/main/java/org/example/Tests/PopupTest.java
+++ src/main/java/org/example/Tests/PopupTest.java
@@ -1,6 +1,15 @@
 package org.example.Tests;
 
+import org.example.Page.Popup;
+import org.testng.annotations.Test;
+
 public class PopupTest {
 
+    Popup popup = new Popup();
+
+    @Test (groups = {"allPopupTest"})
+    public void alert() {
+        popup.alert();
+    }
 
 }
